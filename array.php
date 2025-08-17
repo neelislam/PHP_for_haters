ecff8c05541d46d71e8f8210b57c87193131c778
rabiu <rabiul17.ri@gmail.com>
2025-08-17 16:39:13 +0600

    Update

---
+++ array.php
@@ -0,0 +1,19 @@
+<?php
+echo"I hate PHP <br>";
+echo"Its really good";
+//This is a comment
+/* This is multiline comment */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>Order a pizza</button>
+</body>
+</html>
