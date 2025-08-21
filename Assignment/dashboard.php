5f648fb7e897d939b5ecc2d7190d0a0ef41e3575
rabiu <rabiul17.ri@gmail.com>
2025-08-21 12:12:16 +0600

    Form Design

---
+++ Assignment/dashboard.php
@@ -0,0 +1,28 @@
+<?php 
+$page_title = "Dashboard";
+include('includes/header.php');
+include('includes/navbar.php');
+?>
+<div class="py-5">
+    <div class="containe">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>User Dashboard</h4>
+                    </div>
+                    <div class="card-body">
+                    <h2>
+                    Access when you are logged in
+                    </h2>
+                    <h4>Email Verification</h4>
+
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+
+</div>
+<?php include('includes/footer.php'); ?>
---
+++ Assignment/index.php
@@ -0,0 +1,19 @@
+<?php 
+$page_title = "Home Page";
+include('includes/header.php');
+include('includes/navbar.php');
+?>
+<div class="py-5">
+    <div class="containe">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h2>
+                    Login and Registration System with PHP 
+                </h2>
+                <h4>Email Verification</h4>
+            </div>
+        </div>
+    </div>
+
+</div>
+<?php include('includes/footer.php'); ?>
